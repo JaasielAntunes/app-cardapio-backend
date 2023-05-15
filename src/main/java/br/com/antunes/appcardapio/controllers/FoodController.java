26e1a82fa8f2cc5ee8e70e40eb0cbee28ac84fc7
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-15 01:03:18 -0300

    Ajuste no CORS

--- src/main/java/br/com/antunes/appcardapio/controllers/FoodController.java
+++ src/main/java/br/com/antunes/appcardapio/controllers/FoodController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("food")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "localhost:3000/")
+@CrossOrigin(origins = "http://localhost:5173/, http://localhost:3000/")
 public class FoodController {
 
     @Autowired
--- src/main/java/br/com/antunes/appcardapio/dto/FoodResponseDTO.java
+++ src/main/java/br/com/antunes/appcardapio/dto/FoodResponseDTO.java
@@ -2,7 +2,7 @@ package br.com.antunes.appcardapio.dto;
 
 import br.com.antunes.appcardapio.entities.Food;
 
-public record FoodResponseDTO(Long id, String title, String imgurl, Double price) {
+public record FoodResponseDTO(Long id, String title, String imgUrl, Double price) {
 
     public FoodResponseDTO(Food food) {
         this(food.getId(), food.getTitle(), food.getImgUrl(), food.getPrice());
